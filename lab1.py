a1e61eebb1faca9bd401e7b20beb33619a70bc62
Guns-lingers <artem-kushnarev@mail.ru>
2023-03-09 10:58:45 +0800

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -5,11 +5,9 @@ import uuid, re
 print("PC Name: " + socket.gethostname())
 conn=http.client.HTTPConnection("ifconfig.me")
 conn.request("GET","/ip")
-print("IP: " + str(conn.getresponse().read()))
-
-#print("IP: " + socket.gethostbyname(socket.gethostname()))
+print("External IP: " + str(conn.getresponse().read()))
 
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('77.88.8.8', 80))
-print("IP: " + s.getsockname()[0])
-print(":".join(re.findall('..', '%012x' % uuid.getnode())))
+print("Computer IP: " + s.getsockname()[0])
+print("MAC address: "+":".join(re.findall('..', '%012x' % uuid.getnode())))
