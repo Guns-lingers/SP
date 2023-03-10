6f02828841208c5703872d7ea19087a3b281c4ec
Guns-lingers <artem-kushnarev@mail.ru>
2023-03-10 11:32:58 +0800

    temp commit

--- client.py
+++ client.py
@@ -1,14 +1,21 @@
 import socket
+import threading
+
+def read_sok():
+	while 1:
+		data=sor.recv(1024)
+		print(data.decode('utf8'))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('77.88.8.8', 80))
-
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP=str(s.getsockname()[0])
-
 PORT=12333
-connection.connect((IP, PORT))
-rd=connection.recv(1024)
-print(rd.decode('utf8'))
-connection.send("Hello and you!".encode('utf8'))
-connection.close()
+server = str(s.getsockname()[0]), PORT
+alias=input("input your nickname: ")
+sor=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sor.bind(('',0))
+sor.sendto((alias+' Connect to server').encode('utf8'), server)
+potok=threading.Thread(target=read_sok)
+potok.start()
+while 1:
+	mensahe = input()
+	sor.sendto(('['+alias+']'+mensahe).encode('utf8'), server)
--- server.py
+++ server.py
@@ -3,25 +3,10 @@ import socket
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('77.88.8.8', 80))
 
-listener=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listener=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 IP = str(s.getsockname()[0])
 PORT=12333
+
 listener.bind((IP, PORT))
-
-listener.listen(0)
-
-connection, address=listener.accept()
-
-connection.send("Hello, connect!\n".encode('utf8'))
-
-connection.send("Hello!\n".encode('utf8'))
-
-while True:
-	data_output=''
-	while True:
-		data=connection.recv(1024).decode('utf8')
-		data_output+=data
-		if not data:
-			break
-	print(data_output)
+data, address=listener.recvfrom(1024)
+listener.sendto(data, address)
