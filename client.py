e89bd678c63ca375530b3f4b500e89ca69fbc51e
Guns-lingers <artem-kushnarev@mail.ru>
2023-03-10 10:40:33 +0800

    update client file

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import socket
 
+s = socket.socket(socket.AF_INEt, socket.SOCK_STREAM)
+s.connect(('77.88.8.8', 80))
+
 connection = socket.socket(socket.AF_INEt, socket.SOCK_STREAM)
-IP=""
+IP=str(s.getsockname()[0])
+
 PORT=12333
 connection.connect((IP, PORT))
 rd=connection.recv(1024)
