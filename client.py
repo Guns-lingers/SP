dbc9c6c224dd3ad0a9f874cdacd9fd8e20d757f3
Guns-lingers <artem-kushnarev@mail.ru>
2023-03-10 10:44:51 +0800

    fix bug

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import socket
 
-s = socket.socket(socket.AF_INEt, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('77.88.8.8', 80))
 
-connection = socket.socket(socket.AF_INEt, socket.SOCK_STREAM)
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP=str(s.getsockname()[0])
 
 PORT=12333
--- server.py
+++ server.py
@@ -1,24 +1,27 @@
 import socket
 
-listener=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-IP = socket.gethostbyname(socket.gethostname())
-PORT=12333
-listener.bind((IP, PORT))
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('77.88.8.8', 80))
+print(s.getsockname()[0])
+#listener=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#IP = str(s.getsockname()[0])
+#PORT=12333
+#listener.bind((IP, PORT))
 
-listener.listen(0)
+#listener.listen(0)
 
-connection, address=listener.accept()
+#connection, address=listener.accept()
 
-connection.send("Hello, connect!".encode('utf8'))
+#connection.send("Hello, connect!".encode('utf8'))
 
-connection.send("Hello!".encode('utf8'))
+#connection.send("Hello!".encode('utf8'))
 
-while True:
-	data_output=''
-	while True:
-		data=connection.recv(1024).decode('utf8')
-		data_output+=data
-		if not data:
-			break
-	print(data_output)
+#while True:
+#	data_output=''
+#	while True:
+#		data=connection.recv(1024).decode('utf8')
+#		data_output+=data
+#		if not data:
+#			break
+#	print(data_output)
